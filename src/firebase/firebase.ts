de753109dd5fbc7fbad31cb191fb5063424e2db1
Madhav Kabra <madhav.kabra1100@gmail.com>
2020-02-04 16:00:45 +0530

    Configure and add firebase

---
+++ src/firebase/firebase.ts
@@ -0,0 +1,22 @@
+import * as firebase from 'firebase/app'
+import 'firebase/firestore'
+
+const config = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(config);
+}
+
+const db = firebase.firestore();
+
+export {
+  db,
+};
